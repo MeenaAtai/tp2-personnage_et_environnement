57e37d1df6b0447025b786602efb8fa3047a8fa9
Meena Atai <meenaatai2005@gmail.com>
2025-10-19 00:26:34 -0400

    final

---
+++ menu.gd
@@ -0,0 +1,8 @@
+extends CanvasLayer
+
+
+func _on_jouer_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Jeu.tscn")
+
+func _on_instructions_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/instructions.tscn")
